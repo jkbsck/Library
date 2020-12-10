427c6ff83701654f82ae4e31b5df9a167780a2bc
jkbsck <sucik.jakub@gmail.com>
2020-12-10 21:05:32 +0100

    Library index done

--- js/script.js
+++ js/script.js
@@ -16,19 +16,49 @@ function addBookToLibrary(book) {
 // function to display each book from library
 function displayBooks() {
   booksList = document.getElementById('booksList');
+  // iterate over every book in library
   myLibrary.forEach(element => {
-    console.log(booksList);
     book = document.createElement("div");
     book.classList.add("book");
     booksList.appendChild(book);
-    console.log(element.values);
-    book.textContent = Object.values(element);
+    // create content for name, author, pages and isRead
+    for (let index = 0; index < 4; index++) {
+      h5 = document.createElement("h5");
+      h4 = document.createElement("h4");
+      book.appendChild(h5);
+      book.appendChild(h4);
+      switch (index) {
+        case 0:
+          h5.textContent = "name:";
+          h4.textContent = `${Object.values(element)[index]}`;
+          break;
+        case 1: 
+          h5.textContent = "author:";
+          h4.textContent = `${Object.values(element)[index]}`;
+          break;
+        case 2: 
+          h5.textContent = "pages:";
+          h4.textContent = `${Object.values(element)[index]}`;
+          break;
+        case 3: 
+          h5.textContent = "read?";
+          h4.textContent = `${Object.values(element)[index] == true ? "yes" : "no"}`;
+          break;
+      }
+    }
   });
 }
 
 addBookToLibrary(new Book("Hobbit", "J. R. R. Tolkien", 323, true));
 addBookToLibrary(new Book("Harry Potter", "J. K. Rowling", 563, false));
 addBookToLibrary(new Book("Homo Sapiens", "Yuval N. Harari", 401, true));
+addBookToLibrary(new Book("Communist Manifesto", "Karl Marx", 225, false));
+addBookToLibrary(new Book("Basics of Economics", "Thomas Sowell", 539, true));
 addBookToLibrary(new Book("Communist Manifesto", "Karl Marx", 225, true));
+addBookToLibrary(new Book("Communist Manifesto", "Karl Marx", 225, true));
+addBookToLibrary(new Book("Communist Manifesto", "Karl Marx", 225, true));
+addBookToLibrary(new Book("Communist Manifesto", "Karl Marx", 225, true));
+addBookToLibrary(new Book("Communist Manifesto", "Karl Marx", 225, true));
+
 
 displayBooks();
