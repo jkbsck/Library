1333e6788281def04a485fa25a85c309e456858d
jkbsck <sucik.jakub@gmail.com>
2020-12-11 23:02:49 +0100

    Books can be added to library through form

--- js/script.js
+++ js/script.js
@@ -16,6 +16,9 @@ function addBookToLibrary(book) {
 // function to display each book from library
 function displayBooks() {
   booksList = document.getElementById('booksList');
+  while (booksList.hasChildNodes()) {
+    booksList.removeChild(booksList.lastElementChild);
+  }
   // iterate over every book in library
   myLibrary.forEach(element => {
     book = document.createElement("div");
@@ -49,16 +52,39 @@ function displayBooks() {
   });
 }
 
+// new book form display
+function displayNewBookForm() {
+  newBookForm = document.getElementById("new-book-form");
+  if (newBookForm.classList.contains("new-book-form-hidden")) {
+    newBookForm.classList.remove("new-book-form-hidden");  
+    newBookForm.classList.add("new-book-form");
+  } else if (newBookForm.classList.contains("new-book-form")) {
+    newBookForm.classList.remove("new-book-form");  
+    newBookForm.classList.add("new-book-form-hidden");
+  }
+}
+
+// event listeners
+
+// show 'add new book' form
+var newBookBtn = document.getElementById("new-book-btn");
+newBookBtn.addEventListener("click", displayNewBookForm);
+
+// submit 'add new book' form
+const submitNewBook = document.getElementById("newBookForm");
+submitNewBook.onsubmit = function(e) {
+  e.preventDefault();
+  addBookToLibrary(new Book(submitNewBook.name.value, submitNewBook.author.value, submitNewBook.pages.value, submitNewBook.isRead.value));
+  console.log(submitNewBook.name.value);
+  displayNewBookForm();
+  displayBooks();
+};
+
+// main
 addBookToLibrary(new Book("Hobbit", "J. R. R. Tolkien", 323, true));
 addBookToLibrary(new Book("Harry Potter", "J. K. Rowling", 563, false));
 addBookToLibrary(new Book("Homo Sapiens", "Yuval N. Harari", 401, true));
 addBookToLibrary(new Book("Communist Manifesto", "Karl Marx", 225, false));
 addBookToLibrary(new Book("Basics of Economics", "Thomas Sowell", 539, true));
-addBookToLibrary(new Book("Communist Manifesto", "Karl Marx", 225, true));
-addBookToLibrary(new Book("Communist Manifesto", "Karl Marx", 225, true));
-addBookToLibrary(new Book("Communist Manifesto", "Karl Marx", 225, true));
-addBookToLibrary(new Book("Communist Manifesto", "Karl Marx", 225, true));
-addBookToLibrary(new Book("Communist Manifesto", "Karl Marx", 225, true));
-
 
 displayBooks();
